ee6ec4bd369667451daa266261684651e8f55475
marcelojsil <dev.marcelojs@gmail.com>
2025-01-07 16:43:32 -0300

    site versao 1.0.01

---
+++ views/footer.php
@@ -0,0 +1,29 @@
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="./src/css/style.css" />
+</head>
+<body>
+    
+<footer>
+    <nav class="rodape">
+        <div class="rodape-itens">
+            <img src="./public/img/marthec_1.png" width="250" alt="">
+        </div>
+        <div class="rodape-itens">
+            <ul class="rodape-menu">
+                <li><p>Serviços</p></li>
+                <li><a href="#">Sites Institucionais</a></li>
+                <li><a href="#">Landing Pages</a></li>
+                <li><a href="#">Sites Estáticos</a></li>
+            </ul>
+        </div>
+        <div class="rodape-itens"></div>
+    </nav>
+    <nav>
+        <p>Direitos Reservados - Copyright &copy; 2024 | 2025. Marthec Web - Agência de desenvolvimento e manutenção de sistemas Web</p>
+    </nav>
+</footer>
+
+
+</body>
+</html>
--- wp/wp-content/themes/astra/admin/assets/build/dashboard-app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3e8175a86626f7cd1a4b');
