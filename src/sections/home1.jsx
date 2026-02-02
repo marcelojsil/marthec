8fc679cb6675ec9ab8f8ef64b43105013b239c47
marcelojsil <dev.marcelojs@gmail.com>
2026-02-02 20:11:09 -0300

    melhoras do SEO2

--- src/sections/home1.jsx
+++ src/sections/home1.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             <div className="home-left">
                 <div><h1>Criação de <span className="gradient-text">sites profissionais</span> para empresas que querem crescer</h1></div>
                 <div><h2>Soluções digitais sob medida para pequenas empresas, MEI e prestadores de serviço,
-  focadas em presença online e geração de resultados.</h2></div>
+  focadas em presença online e geração de resultados</h2></div>
                 <div className="buttons">
                     <LinkButton href="#contato" variant="outline">mais informações</LinkButton>
                     <LinkButton href="#pricing" variant="primary">Impulsione seu negócio</LinkButton>
