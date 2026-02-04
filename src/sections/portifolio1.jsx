cf47da1565103d9571406512e6897e848c120094
marcelojsil <dev.marcelojs@gmail.com>
2026-02-04 20:34:34 -0300

    melhorias de SEO e portifólio

--- src/sections/portifolio1.jsx
+++ src/sections/portifolio1.jsx
@@ -2,6 +2,8 @@
 import CardImage from "../components/ui/cardImage";
 import "../styles/portifolio1.css";
 import Eletrotech from "../image/eletrotech.png";
+import Contrutora from "../image/construtora.png";
+import Corretor from "../image/corretor_autonomo.png";
 import Breve from "../image/breve.jpg";
 
 // Ícones simples usando emojis (pode trocar por lucide-react)
@@ -25,16 +27,27 @@ const Portifolio = () => {
           />
           </a>
 
+          <a href="https://construtora-pro.netlify.app/"  rel="noreferrer"  target="_blank">
           <CardImage
-            image={Breve}
-            title="Em Brave"
-            description="Um novo site ou sistema web"
+            image={Contrutora}
+            title="Contrutora Civil"
+            description="Construtora de obras residenciais e comerciais"
             size = {100}
           />
+          </a>
+
+          <a href="https://corretoresdeimoveis.netlify.app"  rel="noreferrer"  target="_blank"> 
+          <CardImage
+            image={Corretor}
+            title="Corretor de Imóveis"
+            description="Corretor de imóveis autônomo"
+            size = {100}
+          />
+          </a>
 
           <CardImage
             image={Breve}
-            title="Em Brave"
+            title="Em Breve"
             description="Um novo site ou sistema web"
             size = {100}
           />
