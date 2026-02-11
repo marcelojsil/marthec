c55d7e208b49c99fc752334a6b3ed3846677f7c4
Marcelo Silva <dev.marcelojs@gmail.com>
2026-02-11 13:22:01 -0300

    ajuste camelCase menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import Menu from './ui/Menu'
+import Menu from './ui/menu'
 import '@/styles/header.css'
 
 import Logo from '../image/logo.png'
